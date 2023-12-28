ed1de635132c964c61dff951828f1a897842e286
Soyoung Park <soyoung008@naver.com>
2023-12-28 16:58:42 +0900

    init : globalstyle 설정

---
+++ timer/src/styles/GlobalStyle.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *{
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    min-width: 136.6rem;
+    width:100%;
+    height: 100vh;
+    margin: 0 auto;
+    font-size: 10px;
+    font-family: "Pretendard";  
+  }
+`;
+
+export default GlobalStyle;
