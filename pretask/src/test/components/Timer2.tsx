5c9ba942b8eb8c2eee5c4117e14b530d23bc7ca5
Soyoung Park <soyoung008@naver.com>
2024-01-03 02:07:02 +0900

    feat : 소요된 시간 계산 및 모달창 뜨도록 구현

--- pretask/src/test/components/Timer2.tsx
+++ pretask/src/test/components/Timer2.tsx
@@ -1,29 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { createTimeModel, useTimeModel } from 'react-compound-timer';
 import styled from 'styled-components';
+import Modal from './Modal';
 
 export interface StopProps {
   isStop: boolean;
 }
 
-const timer = createTimeModel({
-  initialTime: 3601000, //ms 단위(s*1000)
-  direction: 'backward',
-});
-
 export default function Timer2({ isStop }: StopProps) {
+  const initialTime = 3600000;
+  const [timer, setTimer] = useState(
+    createTimeModel({
+      initialTime, //ms 단위(s*1000)
+      direction: 'backward',
+    })
+  );
   const { value, pause } = useTimeModel(timer);
+  const [modal, setModal] = useState(false);
+  const takeTime = initialTime / 1000 - (value.h * 3600 + value.m * 60 + value.s);
+
+  useEffect(() => {
+    setTimer(
+      createTimeModel({
+        initialTime,
+        direction: 'backward',
+      })
+    );
+  }, [initialTime]);
 
   useEffect(() => {
     if (isStop) {
       pause();
+      setModal(true);
     }
   }, [isStop, pause]);
 
   return (
-    <TimerContainer>
-      {`0${value.h}`}:{value.m < 10 ? `0${value.m}` : value.m}:{value.s < 10 ? `0${value.s}` : value.s}
-    </TimerContainer>
+    <>
+      <TimerContainer>
+        {`0${value.h}`}:{value.m < 10 ? `0${value.m}` : value.m}:{value.s < 10 ? `0${value.s}` : value.s}
+      </TimerContainer>
+      {modal && <Modal setModal={setModal} takeTime={takeTime} />}
+    </>
   );
 }
 const TimerContainer = styled.section`
